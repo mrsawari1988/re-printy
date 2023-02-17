e3dadf10b40cf788ab1e835246a3451de00c34cf
mrsawari1988 <m.r.sawary@gmail.com>
2023-02-17 22:28:02 +0330

    hooks & components converted to ts

---
+++ src/components/Contorols.tsx
@@ -0,0 +1,74 @@
+import React, { useState } from 'react';
+import { useRef } from 'react';
+import useControls from '../hooks/useControls';
+import useValidation from './../hooks/useValidation';
+import { error, fileItem } from '@/types';
+export default function Contorols({ addFileHandler, printersList }) {
+    // refrence to the file input so later it's value will be cleared
+    const fileRef = useRef<HTMLInputElement>(null);
+    const { state, changeHandler, buttonName, isActive, resetState } = useControls(fileRef);
+    const { validator } = useValidation(fileRef);
+    const [errors, setErrors] = useState<error[] | []>([]);
+    const addFile = () => {
+        //input validations
+        const failores = validator(state);
+        if (failores.length > 0) {
+            setErrors([...failores]);
+        } else {
+            addFileHandler({
+                fileName: state.fileName,
+                printerName: state.printerName,
+                filePath: state.filePath,
+                copies: state.copies,
+                id: Math.random(),
+            });
+
+            resetState();
+            setErrors([]);
+        }
+    };
+    return (
+        <div className='contorols'>
+            <div className='adding-contorols'>
+                <label className={isActive ? 'file-upload active' : 'file-upload'}>
+                    <input
+                        type='file'
+                        onChange={(e) => changeHandler(e)}
+                        ref={fileRef}
+                        accept='application/pdf'
+                    />
+                    {buttonName}
+                </label>
+                <select name='printerName' id='printer' onChange={(e) => changeHandler(e)}>
+                    {/* render printers list */}
+                    {printersList &&
+                        printersList.map((printer) => (
+                            <option value={printer.name} key={printer.deviceId}>
+                                {printer.name}
+                            </option>
+                        ))}
+                </select>
+                <div className='print-number'>
+                    <label htmlFor='copies'>Number of Copies</label>
+                    <input
+                        type='text'
+                        name='copies'
+                        value={state.copies || 1}
+                        onChange={(e) => changeHandler(e)}
+                    />
+                </div>
+                <div className='errors'>
+                    {errors &&
+                        errors.map((error) => (
+                            <p className='alert' key={error.id}>
+                                - {error.message}
+                            </p>
+                        ))}
+                </div>
+            </div>
+            <div className='add-item-button'>
+                <button onClick={addFile}>Add</button>
+            </div>
+        </div>
+    );
+}
